f0e06e2ab0d5260045c212fe56fe3e1e4f0fda61
Esrael Geremew <59869833+esrael-github@users.noreply.github.com>
2024-06-08 14:16:57 -0700

    create FSR data models (i.e. flight chart and passengers queue) (#10)

--- app.js
+++ app.js
@@ -1,206 +1,40 @@
 /**
- * Cotributed by Esrael Geremew 
- * IMPLMENTATION TIPS: Mimorization approach is used to improves the perforamce of a program
- *  by storing previously processed values.
- * Total Space Complexity = Input Space + Auxiliary Space(global+local)
- */
-let seatInputs = [], pQueue, flight = [];//Input Space - list of variables for I/O storage 
+ * Introduction: This commit bulids data models of the FSR algorithm:-
+ *      [1] Flight Chart (row-major) model, and 
+ *      [2] Passengers Queue (FIFO) model
+ * Technology: Node.js (back-end)
+ * Required User Inputs:
+ *      Example Seat Input (Col-major): [[3,2], [4,3], [2,3], [3,4]]
+ *      Number of Passengers: Enter any +ve intiger number 
+ * Contributed by: Esrael G.
+ **/
+let seatInputs = [], pQueue = [], fChart = [];//Input Space - list of variables for I/O storage 
+//Axulary Space - list of (global variables) for computation:
+let totalSeats = 0, resultSeat = [], rsCatList, i = 0, j; //data processing 
+let nextType = 0, cat, row, col = -1, nextPID = 0; //Mimorization approach
+let myCounter = 0, rsCounter = 0;//uitilized in latter implmentaion 
 
-let totalSeats = 0, resultSeat = [], rsCounter = 0, i = 0, j; //Axulary Spaces FOR data processing 
-let nextType = 0, cat, row, col = -1, nextPID = 0; //Axulary Spaces to adopt Mimorization approach
-
-let myCounter = 0, app_status_inicator = 0;//Axulary Spaces reserved for app status checking
-
-//A function to get the next aviliable seat attributs
-function nextSeat() {
-    myCounter++;
-    if (col === -1) {
-        resultSeat = [];
-        if (nextType === 1) { // Test cases for 'Window type' seats
-            resultSeat.push(0);
-            rsCounter += seatInputs[0][0];
-            if (seatInputs.length > 1) {
-                resultSeat.push(seatInputs.length - 1);
-                rsCounter += seatInputs[seatInputs.length - 1][0];
-            } else {
-                if (seatInputs[0][1] > 1) { rsCounter += seatInputs[0][0] };
-            };
-        } else {
-            for (i = 0; i < seatInputs.length; i++) { //inner loop
-                if (nextType === 0) {//Test cases for 'Aisle type' seats  
-                    if (seatInputs.length > 1) {
-                        if (i === 0 || i === seatInputs.length - 1) {
-                            if (seatInputs[i][1] > 1) {
-                                resultSeat.push(i);
-                                rsCounter += seatInputs[i][0];
-                            }
-                        } else {
-                            resultSeat.push(i);
-                            if (seatInputs[i][1] > 1) {
-                                rsCounter += (seatInputs[i][0] * 2);
-                            } else {
-                                rsCounter += seatInputs[i][0];
-                            }
-                        }
-                    };
-                } else { //if (nextType === 2) { Test cases for 'Middel type' seats
-                    if (seatInputs[i][1] > 2) {
-                        resultSeat.push(i);
-                        rsCounter += (seatInputs[i][0] * (seatInputs[i][1] - 2));
-                    };
-                }
-            }
-        }
-        if (resultSeat.length > 0) {
-            j = row = 0;
-            cat = resultSeat[j];
-            if (nextType === 0) {
-                if (cat === 0) {
-                    col = seatInputs[cat][1] - 1;
-                } else { col = 0 };
-            } else if (nextType === 1) {
-                col = 0;
-            } else { col = 1 };
-            console.log('[Next Seat*] Type:' + nextType + ', Result:' + rsCounter + '_per_' + totalSeats);
-        } else {
-            console.log('[Next Seat*] Type:' + nextType + ', Result: No');
-            nextType += 1;
-            nextSeat();
-        }
-    } else {
-        if (nextType === 0) {//Aisle seat cell test cases
-            if (cat === seatInputs.length - 1) {
-                j = 0, row += 1;
-                cat = resultSeat[j];
-                if (cat === 0) {
-                    col = seatInputs[cat][1] - 1;
-                } else {
-                    col = 0;
-                }
-            } else {
-                if (col === seatInputs[cat][1] - 1) {
-                    if (j === resultSeat.length - 1) {
-                        j = 0, row += 1;
-                        cat = resultSeat[j];
-                        if (cat === 0) {
-                            col = seatInputs[cat][1] - 1
-                        } else {
-                            col = 0;
-                        }
-                    } else {
-                        cat = resultSeat[++j];
-                        col = 0;
-                    }
-                } else {
-                    col = seatInputs[cat][1] - 1;
-                }
-            }
-            if (seatInputs[cat][0] <= row) { nextSeat() };
-        } else if (nextType === 1) {//window seat cell test cases
-            if (resultSeat.length > 1) {
-                if (j === 0) {
-                    cat = resultSeat[++j];
-                    col = seatInputs[cat][1] - 1;
-                } else {
-                    cat = resultSeat[--j];
-                    row += 1, col = 0;
-                }
-            } else {
-                if (col === seatInputs[cat][1] - 1) {
-                    row += 1, col = 0;
-                } else {
-                    col = seatInputs[cat][1] - 1;
-                }
-            }
-            if (seatInputs[cat][0] <= row) { nextSeat() };
-        } else if (nextType === 2) { //middle seat test cases
-            if (col === seatInputs[cat][1] - 2) {
-                col = 1;
-                if (j === resultSeat.length - 1) {
-                    j = 0;
-                    cat = resultSeat[j];
-                    row += 1;
-                } else {
-                    cat = resultSeat[++j];
-                }
-            } else {
-                col += 1;
-            }
-            if (seatInputs[cat][0] <= row) { nextSeat() };
-        }
-    }
-}//end of nextSet func.
-let View = {
-    getFlightStatus: function (vReq_msg) {
-        let status_aler = 'Flight Status:->';
-        if (app_status_inicator === 0) {
-            status_aler += 'Data Processing ...';
-            console.log(status_aler + '\n' + 'Flight Chart:\n', flight, '\n' + vReq_msg);
-        } else if (app_status_inicator === 1) {
-            status_aler += 'Rady ...';
-            console.log(status_aler + vReq_msg);
-            console.log('[Row-major] Flight Seatting:\n', flight);
-            console.log("App Time Complexity =", myCounter);
-        } else {
-            status_aler += 'Not Rady ...';
-            console.log(status_aler + vReq_msg + '!');
-        }
-    }
-};
-//An object to get inputs and create flight model
-let C = {//C-ontroler component 
+let model = { //an object to get inputs and create flight model
     init: function () {
-        View.getFlightStatus('Set Flight Data!')
-        const rl = require('readline-sync'); //thrid-part liberary for user inputs
-        //Get user input-1: list of columen-major seat inputs
-        console.log(`Sample Seat Inputs(Col-major) :`, [[3, 2], [4, 3], [2, 3], [3, 4]]);
-        let stringInput = String(rl.question('[Column-major] Enter Seat Inputs:'));
-        arrayInput = C.parseInput(stringInput); // Parse string input to array
-        //Get user input-2: number of passengers wating in Queue
-        let queLent = Number(rl.question(`Enter Number of Passengers:`));//length of pQueue
-        //Check the validation of user inputs 
-        if (C.isInputValid(arrayInput, queLent) === false) {
-            View.getFlightStatus('Terminated');
-            return false
-        };
-        //Generate flight and queue models
+        const rl = require('readline-sync'); //thrid-part liberary for accepting user input
+        let stringInput, qLent;
+        console.log(`Example Seat Inputs(Col-major):`, [[3, 2], [4, 3], [2, 3], [3, 4]]);
+        stringInput = String(rl.question('Enter Seat Inputs:'));//Get seat inputs
+        qLent = Number(rl.question(`Enter Number of Passengers:`));//Any +ve intiger number
+
+        arrayInput = model.parseInput(stringInput); // Parse string input to array
+        model.isInputValid(arrayInput, qLent)//verifies user inputs 
+        //Generate Fligh Chart model
         arrayInput.forEach(myInput => {
             seatInputs.push([myInput[1], myInput[0]]); //generat row-major seat-inputs
-            totalSeats += (myInput[1] * myInput[0]);
-            //generate new seat model
             newSeat = new Array(myInput[1]).fill().map(() => new Array(myInput[0]).fill(null))
-            //Update flight (model) chart
-            flight.push(newSeat);  
+            fChart.push(newSeat); //add new set to flight chart
+            totalSeats += (myInput[1] * myInput[0]);
         });
-        pQueue = Array(queLent).fill().map(() => (
-            { Name: null, PID: ++nextPID, Seat: null }));
-        if (C.assignNextFlight() === 1) {//Initialize Flight Seatting Algorithm
-            app_status_inicator++;
-            View.getFlightStatus('DONE, Flight Seat Reserved!');
-        } else {
-            View.getFlightStatus('System Error, Try agin !');
-        }
+        //Generate Passengers Queue (FIFO) as a dictionary
+        pQueue = Array(qLent).fill().map(() => ({ Name: null, PID: ++nextPID, Seat: null }));
     },
-    //A function to assign seats as order of aisle -> window -> middle type
-    assignNextFlight: function () {
-        if (pQueue.length > totalSeats) {
-            console.log(`=> Only the 1st `+ totalSeats +` passengers will be able to fly away.`)
-        };
-        while (pQueue.length != 0) {
-            nextPID = pQueue[0].PID;
-            nextSeat(); //get next Seat cat,row and col 
-            flight[cat][row][col] = nextPID; //asign the resulted seat to the passenger in queue
-            console.log('[Seat]->[', cat, row, col, ']<=>[PID]->[', nextPID, ']');
-            pQueue = pQueue.slice(1, pQueue.length)//FIFO princeple on Array elements with O(1)
-            if (nextPID === totalSeats) { return 1 };
-            if (--rsCounter === 0) {
-                console.log('.., reserved sucessfuly.')
-                nextType += 1, col = -1;
-            };
-        }
-        return 1;
-    },
-    //Get a string from input in the format '[[x,y],[x,y]]' and return an array [[x,y],[x,y]]
+    //Accept a string from input in the format '[[x,y],[x,y]]' and return an array [[x,y],[x,y]]
     parseInput: function (strInput) {
         strInput = strInput.replace(/\s/g, ''); //remove extra spaces
         strInput = strInput.substring(2, strInput.length - 2); //remove first and last 2 symbol
@@ -216,31 +50,34 @@ let C = {//C-ontroler component
     },
     isInputValid: function (seatInputs, passengers) {
         if (seatInputs.length > 8) {
-            console.log('Too many sections with the rows and columns!\n');
+            console.log('Too many sections with the rows and columns!');
             return false;
         };
         for (i = 0; i < seatInputs.length; i++) {
             for (j = 0; j < seatInputs[i].length; j++) {
                 if (seatInputs[i][j] < 1) {
-                    console.log('The rows and columns must be more than 0!\n');
+                    console.log('The rows and columns must be more than 0!');
                     return false;
                 };
                 if (Number.isNaN(seatInputs[i][j])) {
-                    console.log('The rows and columns must be a number!\n');
+                    console.log('The rows and columns must be a number!');
                     return false;
                 };
             }
         }
         if (passengers < 1 || passengers % 1 != 0) {
-            console.log('Incorrect input!\n');
+            console.log('Incorrect input!');
             return false;
         };
     }
 }
-C.init();//Driver codes
 
-/**
- * Version: 1.0.1
- * Improvment: validate wrong seat inputs (Fix bugs)
- * Time complexity: Ordere of Passengers to one [O(P) Vs. O(1)] 
- */
+//Driver codes 
+model.init();
+if (fChart.length !== 0 && pQueue.length !== 0) {
+    console.log(`\nPassengers Queue [FIFO]:`)
+    console.table(pQueue);
+    console.log(`\nFlight Chart [row-major]:\n`, fChart)
+} else {
+    console.log("Data Encoding Error, try agin!")
+}
